22f5f73b34d6ef8833d9bc8e09f997f90df1b06a
Ted Kassen <tedgkassen@gmail.com>
2021-09-01 00:44:50 +0900

    Impl sqlite storage

---
+++ src/irasutoya/db.clj
@@ -0,0 +1,42 @@
+(ns irasutoya.db
+  (:require [ragtime.jdbc :as ragtime]
+            [ragtime.repl :as repl]
+            [next.jdbc :as jdbc]
+            [honey.sql :as honey]
+            [inflections.core :as inflections]))
+
+(def config
+  {:dbtype "sqlite"
+   :classname "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname "irasutoya.db"
+   :dbname "irasutoya.db"})
+
+(defn migrate []
+  (repl/migrate
+   {:datastore (ragtime/sql-database config)
+    :migrations (ragtime/load-directory "migrations")}))
+
+(defn- clj-ify [result]
+  (some->> result
+           vec
+           (map (fn [[a b]]
+                  [(inflections/hyphenate a) b]))
+           (into {})))
+
+(defn execute-one! [sql]
+  (clj-ify (jdbc/execute-one! config (honey/format sql) {:return-keys true})))
+
+(defn insert-all! [table values]
+  (execute-one! {:insert-into table
+                 :values values}))
+
+(defn- tag-data
+  [{:keys [id tags]}]
+  (map #(hash-map :image-id id :value %) tags))
+
+(defn insert-image-data!
+  [entries]
+  (insert-all! :images (map #(dissoc % :tags) entries))
+  (insert-all! :tags (mapcat tag-data entries))
+  entries)
